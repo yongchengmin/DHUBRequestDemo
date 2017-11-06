3c1023e63f922cdf56fd7d4cce652d97db06d4f8
yongchengmin <minyongcheng@qq.com>
2017-11-06 08:39:25 +0800

    first demo for odd
    first demo for odd

---
+++ src/com/jac/odd/demo/test/DemoTest.java
@@ -0,0 +1,70 @@
+package com.jac.odd.demo.test;
+
+import java.util.HashMap;
+import java.util.Map;
+import net.sf.json.JSONObject;
+import com.jac.odd.demo.utils.DataUtils;
+import com.jac.odd.demo.utils.UnicodeUtils;
+import com.jac.odd.demo.utils.requestUtil;
+public class DemoTest {
+	private static String URL = "http://192.168.10.92:8080/odd_server/odd_server?";//测试地址,根据实际提供地址修改
+	private static String URL_LOCALHOST = "http://localhost:8086/odd/odd_server?";//本地测试地址
+	private static String URL_OUT = "http://220.178.49.203:9088//odd_server/odd_server?";//测试外网地址,根据实际提供地址修改
+	private static String ACTION_QUERY = "actionQuery";
+	private static String method = "test_name";//根据实际需求修改
+	private static String airm = "insert";
+	public static void main(String[] args) throws Exception {
+		//data主业务数据业务逻辑
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		dataMap.put("order_no", "订单号");
+		dataMap.put("dealer_code", "经销商编码");
+		dataMap.put("driver_name", "司机姓名");
+		//转换成json格式
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.putAll(DataUtils.convertObjectToStringParams(dataMap));
+		String postData = jsonObject.toString();
+		System.out.println("NO1:"+postData);
+		//对JSON内容进行ENCODE转换
+		postData = UnicodeUtils.toUnicodeString(postData);
+		System.out.println("NO2:"+postData);
+		
+		//invoke type01
+//		String html = requestUtil.getResponseData(URL+requestData(ACTION_QUERY, method, airm)+postData, postData);
+		
+		//invoke type02
+		String html = requestUtil.post(DataUtils.convertObjectToStringParams(getParamMap(postData)), URL_LOCALHOST);
+		
+		System.out.println("NO4:"+html);
+	}
+	
+	private static String requestData(String interfaceType,String method,String airm) {
+		StringBuffer urlBuf = new StringBuffer();
+		urlBuf.append("interfaceType=");
+		urlBuf.append(interfaceType);
+		urlBuf.append("&action=");
+		urlBuf.append(method);
+		urlBuf.append("&airm=");
+		urlBuf.append(airm);
+		urlBuf.append("&data=");
+		return urlBuf.toString();
+	}
+	private static Map<String, Object> getParamMap(String postData){
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("interfaceType", ACTION_QUERY);
+		paramMap.put("action", method);
+		paramMap.put("airm",airm);
+		paramMap.put("data", postData);
+		return paramMap;
+	}
+	/**
+	>>>SUCESS MESSAGE
+	NO1:{"driver_name":"司机姓名","dealer_code":"经销商编码","order_no":"订单号"}
+	NO2:{"driver_name":"\u53f8\u673a\u59d3\u540d","dealer_code":"\u7ecf\u9500\u5546\u7f16\u7801","order_no":"\u8ba2\u5355\u53f7"}
+	NO4:{"code":101,"message":"success"}
+	
+	>>>ERROR MESSAGE
+	NO4:{"code":404,"message":"网络连接超时！"}
+	NO4:{"code":202,"message":"URL IS ERROR！"}
+	NO4:FORBIDDEN!
+	 */
+}
---
+++ src/com/jac/odd/demo/utils/requestUtil.java
@@ -0,0 +1,188 @@
+package com.jac.odd.demo.utils;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.json.JSONObject;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+public class requestUtil {
+
+	public static final String ENCODING = "UTF-8";
+	
+	private final static String CONTENT_TYPE_APPLICATION_JSON = "application/json";
+
+
+	public static String post(Map<String, String> paramMap, String url) throws Exception {
+		String result = null;
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpPost httpPost = new HttpPost(url);
+			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+			for (String key : paramMap.keySet()) {
+				nvps.add(new BasicNameValuePair(key, paramMap.get(key)));
+			}
+			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(10000)
+					.setConnectionRequestTimeout(10000).setSocketTimeout(30000).build();
+			httpPost.setConfig(requestConfig);
+			httpPost.setEntity(new UrlEncodedFormEntity(nvps, ENCODING));
+			CloseableHttpResponse response = httpclient.execute(httpPost);
+			HttpEntity entity = null;
+			try {
+				if (response != null) {
+					entity = response.getEntity();
+					if (entity != null) {
+						result = EntityUtils.toString(entity, ENCODING);
+					}
+				}
+				System.out.println(response.getStatusLine());
+				// do something useful with the response body
+				// and ensure it is fully consumed
+
+				EntityUtils.consume(entity);
+			} finally {
+				response.close();
+			}
+		} finally {
+			httpclient.close();
+		}
+
+		return result;
+	}
+
+	public static String get(Map<String, String> paramMap, String url) throws Exception {
+		String str = "";
+		String result = null;
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+			for (String key : paramMap.keySet()) {
+				nvps.add(new BasicNameValuePair(key, paramMap.get(key)));
+			}
+			str = EntityUtils.toString(new UrlEncodedFormEntity(nvps, ENCODING));
+			HttpGet httpGet = new HttpGet(url + "?" + str);
+			CloseableHttpResponse response = httpclient.execute(httpGet);
+			HttpEntity entity = null;
+			try {
+				if (response != null) {
+					entity = response.getEntity();
+					if (entity != null) {
+						result = EntityUtils.toString(entity, ENCODING);
+					}
+				}
+				System.out.println(response.getStatusLine());
+				// do something useful with the response body
+				// and ensure it is fully consumed
+
+				EntityUtils.consume(entity);
+			} finally {
+				response.close();
+			}
+		} finally {
+			httpclient.close();
+		}
+
+		return result;
+	}
+	
+	
+	public static String postRequest(String url, Map<String, Object> param) throws Exception {
+		String result = "";
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpPost httpPost = new HttpPost(url);
+			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(10000)
+					.setConnectionRequestTimeout(10000).setSocketTimeout(30000).build();
+			httpPost.setConfig(requestConfig);
+			httpPost.setHeader("Content-Type", "application/json;charset=UTF-8");
+
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.putAll(DataUtils.convertObjectToStringParams(param));
+			String parameter = jsonObject.toString();
+
+			StringEntity se = new StringEntity(parameter,ENCODING);
+			se.setContentType(CONTENT_TYPE_APPLICATION_JSON);
+			httpPost.setEntity(se);
+
+			CloseableHttpResponse response = httpclient.execute(httpPost);
+			HttpEntity entity = null;
+			try {
+				if (response != null) {
+					entity = response.getEntity();
+					if (entity != null) {
+						result = EntityUtils.toString(entity, ENCODING);
+					}
+				}
+				EntityUtils.consume(entity);
+			} finally {
+				response.close();
+			}
+		} finally {
+			httpclient.close();
+		}
+		return result;
+	}
+	public static String getResponseData(String url, String postData)throws Exception{
+		String data = null;
+		DataOutputStream wr = null;
+		DataInputStream rd = null;
+		HttpURLConnection conn = null;
+		try {
+			System.out.println("NO3:"+url);
+			URL dataUrl = new URL(url);
+			conn = (HttpURLConnection) dataUrl.openConnection();
+			conn.setConnectTimeout(100000);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Proxy-Connection", "Keep-Alive");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			OutputStream os = conn.getOutputStream();
+			wr = new DataOutputStream(os);
+			wr.write(postData.getBytes());
+			wr.flush();
+			wr.close();
+			InputStream is = conn.getInputStream();
+			rd = new DataInputStream(is);
+			byte d[] = new byte[rd.available()];
+			rd.read(d);
+			data = new String(d);
+			conn.disconnect();
+		}  catch(ConnectException e) {
+        	return "{\"code\":404,\"message\":\"Read timed out£¡\"}";
+        } finally {
+            if (rd != null) {
+                rd.close();
+                rd = null;
+            }
+            if (wr != null) {
+                wr.close();
+                wr = null;
+            }
+            if (conn != null) {
+            	conn.disconnect();
+                conn = null;
+            }
+        }
+		return data;
+	}
+}
